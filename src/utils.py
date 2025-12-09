1c6c9b862f0a71d988f425ab8c58a6c5f8385408
nicolas-puskovic <puskovic.n@gmail.com>
2025-12-09 17:44:55 +0100

    Update utils.

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,7 @@
 import pandas as pd
 
 def to_daily_resolution(df_monthly, method='linear'):
-    """
-    Centralizza la logica di conversione da frequenza Mensile a Giornaliera.
-    Usa interpolazione LINEARE per evitare overshooting sui prezzi (sicurezza finanziaria).
-    """
+    
     # Resample giornaliero crea righe vuote (NaN) tra i mesi
     df_daily = df_monthly.resample('D').mean()
     
@@ -13,4 +10,5 @@ def to_daily_resolution(df_monthly, method='linear'):
     # che non esistono (a differenza della 'cubic').
     df_interpolated = df_daily.interpolate(method=method)
     
+
     return df_interpolated
